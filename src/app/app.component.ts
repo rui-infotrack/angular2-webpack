47d1b282ff8aa0c632f5a2ab93a13ec26a70968e
ktei <ktei2008@gmail.com>
2016-08-21 00:04:46 +1000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { UserService } from '../services/UserService';
+import '../../public/css/styles.css';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  user = '';
+
+  constructor(userService: UserService) {
+    this.user = userService.userName;
+  }
+}
